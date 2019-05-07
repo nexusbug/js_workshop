55f25e29802949d9181dfdf1d960e3d2ac8cae9b
nexusbug <29239366+nexusbug@users.noreply.github.com>
2019-05-07 21:56:54 +0300

    07 may 2019

--- may_05_19/010/sketch.js
+++ may_05_19/010/sketch.js
@@ -5,18 +5,24 @@ let y = 300;
 let btn;
 let bgcolor;
 let sldr;
+let nameInput;
+let nameP;
 
 
 function setup() {
   canvas = createCanvas(600, 600);
   h1 = createElement("h2", "title by js");
   createP("this is a paragraph created by js");
+  nameP = createP("your mouse");
+  nameP.mouseOver(overPara);
+  nameP.mouseOut(outPara);
   // canvas.position(0, 0);
   btn = createButton("light switch");
   bgcolor = 255;
   btn.mousePressed(changeColor);
   createP();
   sldr = createSlider(0, 255, bgcolor);
+  nameInput = createInput("type your name");
 
 }
 
@@ -27,11 +33,14 @@ function draw() {
   fill(255, 0, 0);
   rect(200, 400, 30, 50);
   ellipse(400, 200, 30, 50);
-
+  bgcolor = sldr.value();
   x += random(-3, 3);
   y += random(-3, 3);
 
   h1.position(x - 150, y - 150);
+
+  text(nameInput.value(), 100, 300);
+  // nameP.html(nameInput.value());
 }
 
 function mousePressed() {
@@ -42,12 +51,24 @@ function mousePressed() {
 
 function changeColor() {
   bgcolor = random(255);
+  // console.log(bgcolor);
+  sldr.value(bgcolor);
+}
+
+function overPara() {
+  nameP.html("your mouse is over me");
+
+}
+
+function outPara() {
+  nameP.html("your mouse is out of me");
+
 }
 
 
 
 /**
-05 may 2019
+07 may 2019
 
 first day
 
@@ -55,6 +76,7 @@ fast rust removal continued
 
 which subjets recovered:
 
-21) html, dom,
+21) html, dom
+22) css
 
 */
